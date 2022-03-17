d8b626c4799760465d5e5efc6bcd207f448af756
yulaKochina <yulya7@inbox.ru>
2022-03-17 21:00:00 +0300

    Внесла изменения в текст программы

--- Sandbox/src/main/java/MyFirstProgram.java
+++ Sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,5 @@
 public class MyFirstProgram{
   public static void main(String[] args){
-     System.out.println("Hello, world");
+     System.out.println("It's my first programm! \nHello, world");
 }
 }
