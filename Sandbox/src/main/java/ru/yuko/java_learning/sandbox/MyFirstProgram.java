f18d631aaacdee32effcb447a6841d9f1598f4eb
yulaKochina <yulya7@inbox.ru>
2022-03-18 22:50:12 +0300

    Добавление нескольких функций

--- Sandbox/src/main/java/ru/yuko/java_learning/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/yuko/java_learning/sandbox/MyFirstProgram.java
@@ -1,7 +1,33 @@
 package ru.yuko.java_learning.sandbox;
 
 public class MyFirstProgram {
-  public static void main(String[] args){
-     System.out.println("It's my first program! \nHello, world");
-}
+    public static void main(String[] args) {
+        // Опробовала использование выражений
+        String text = "Hello, world!";
+        System.out.println("\nIt's my first program! " + text);
+
+        // Вызвала функцию осуществляющую вывод текста с использованием значения передаваемого фукции
+        String txt = "Yuliya";
+        text(txt);
+
+        // Определина значения переменных
+        double a = 3;
+        double b = 5;
+        String area = "\nПлощадь прямоугольника со сторонами " + a + " и " + b + " равна ";
+
+        // Вычисление и вывод площанди через выражения
+        double s = a * b;
+        System.out.println("\n(площадь через выражение) " + area + "= " + s);
+
+        // Вычисление и вывод площанди через функцию
+        System.out.println("\n(площадь через функцию) " + area + "= " + area(a,b));
+    }
+
+    public static void text( String txt){
+        System.out.println("\nHi, " + txt + ", it's first program!");
+    }
+
+    public static double area(double a, double b){
+        return a * b;
+    }
 }
