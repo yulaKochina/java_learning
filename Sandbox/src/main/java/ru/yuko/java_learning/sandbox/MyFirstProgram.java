9daa86530ce1567ea684019af7430d0478addc1a
yulaKochina <yulya7@inbox.ru>
2022-03-18 10:04:57 +0300

    Создан пакет для первой программы

--- Sandbox/src/main/java/ru/yuko/java_learning/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/yuko/java_learning/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram{
+package ru.yuko.java_learning.sandbox;
+
+public class MyFirstProgram {
   public static void main(String[] args){
      System.out.println("It's my first programm! \nHello, world");
 }
