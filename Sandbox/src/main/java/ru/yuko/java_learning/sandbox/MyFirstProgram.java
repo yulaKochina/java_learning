5bfdf47af5f8c7ae786fbd6c53dbbf8e67b50d27
yulaKochina <yulya7@inbox.ru>
2022-03-18 20:59:59 +0300

    Скорректировала текст первой программы

--- Sandbox/src/main/java/ru/yuko/java_learning/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/yuko/java_learning/sandbox/MyFirstProgram.java
@@ -2,6 +2,6 @@ package ru.yuko.java_learning.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args){
-     System.out.println("It's my first programm! \nHello, world");
+     System.out.println("It's my first program! \nHello, world");
 }
 }
