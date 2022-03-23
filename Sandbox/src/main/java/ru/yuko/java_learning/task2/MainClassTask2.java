36e35d0ab6de6ecfbb1d7c4db2c5cf343b05602c
yulaKochina <yulya7@inbox.ru>
2022-03-24 01:37:24 +0300

    удалила лишние комментарии

--- Sandbox/src/main/java/ru/yuko/java_learning/task2/MainClassTask2.java
+++ Sandbox/src/main/java/ru/yuko/java_learning/task2/MainClassTask2.java
@@ -5,8 +5,6 @@ public class MainClassTask2 {
 
         Point p1 = new Point(2, 0);
         Point p2 = new Point(4, 0);
-        //String txt = System.out.println("\n√|((" + p1.x + "-" + p2.x + ")*2)+((" + p1.y + "-" + p2.y + ")*2)|");
-
         System.out.println("Рассояние между двумя точками : " +  txt(p1,p2) + " = " + distance(p1, p2));
     }
     public static String txt(Point p1, Point p2){
