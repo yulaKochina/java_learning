c9d2dc70ab44e394fc79e09771bece234e1a1523
yulaKochina <yulya7@inbox.ru>
2022-03-24 01:31:54 +0300

    Добавление выполнения задания 2 из урока 1, создала отдельную директория для данного задания,, создала класс point, создала запускаемый класс, добавила в него функцию по вычислению расстояния между двумя точкам

---
+++ Sandbox/src/main/java/ru/yuko/java_learning/task2/MainClassTask2.java
@@ -0,0 +1,19 @@
+package ru.yuko.java_learning.task2;
+
+public class MainClassTask2 {
+    public static void main(String[] args) {
+
+        Point p1 = new Point(2, 0);
+        Point p2 = new Point(4, 0);
+        //String txt = System.out.println("\n√|((" + p1.x + "-" + p2.x + ")*2)+((" + p1.y + "-" + p2.y + ")*2)|");
+
+        System.out.println("Рассояние между двумя точками : " +  txt(p1,p2) + " = " + distance(p1, p2));
+    }
+    public static String txt(Point p1, Point p2){
+        return "√|((" + p1.x + "-" + p2.x + ")*2)+((" + p1.y + "-" + p2.y + ")*2)|";
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.abs(((p1.x - p2.x) * 2) + ((p1.y - p2.y) * 2)));
+    }
+}
---
+++ Sandbox/src/main/java/ru/yuko/java_learning/task2/Point.java
@@ -0,0 +1,11 @@
+package ru.yuko.java_learning.task2;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
