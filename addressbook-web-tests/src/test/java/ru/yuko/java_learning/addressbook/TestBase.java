b33dad2d1bf4383c0daccd089b8ddb0757cd5fc6
yulaKochina <yulya7@inbox.ru>
2022-04-03 23:42:34 +0300

    Создание общего класса TastBase и перенесение вспомогательных методов из класса GroupCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/TestBase.java
@@ -0,0 +1,90 @@
+package ru.yuko.java_learning.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    private WebDriver wd;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+      System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
+      wd = new ChromeDriver();
+      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      wd.get("http://localhost/addressbook/group.php");
+      login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      wd.findElement(By.name("user")).click();
+      wd.findElement(By.name("user")).clear();
+      wd.findElement(By.name("user")).sendKeys(username);
+      wd.findElement(By.name("pass")).click();
+      wd.findElement(By.name("pass")).clear();
+      wd.findElement(By.name("pass")).sendKeys(password);
+      wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    protected void returnToGroupPage() {
+      wd.findElement(By.linkText("groups")).click();
+    }
+
+    protected void submitGroupCreation() {
+      wd.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+      wd.findElement(By.name("group_name")).click();
+      wd.findElement(By.name("group_name")).clear();
+      wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
+      wd.findElement(By.name("group_header")).click();
+      wd.findElement(By.name("group_header")).clear();
+      wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
+      wd.findElement(By.name("group_footer")).click();
+      wd.findElement(By.name("group_footer")).clear();
+      wd.findElement(By.name("group_footer")).sendKeys(groupData.getGropFouter());
+    }
+
+    protected void initGroupCreation() {
+      wd.findElement(By.name("new")).click();
+    }
+
+    protected void gotoGroupPage() {
+      wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+      logout();
+      wd.quit();
+    }
+
+    private void logout() {
+      wd.findElement(By.linkText("Logout")).click();
+    }
+
+    private boolean isElementPresent(By by) {
+      try {
+        wd.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+
+    private boolean isAlertPresent() {
+      try {
+        wd.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+}
