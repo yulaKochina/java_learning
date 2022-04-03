269dcf1904c27a20aa69b5e3ea1e250eb06dd591
yulaKochina <yulya7@inbox.ru>
2022-04-03 20:31:03 +0300

    Добавила тест на создание нового контакт

---
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/ContactCreationTests.java
@@ -0,0 +1,154 @@
+package ru.yuko.java_learning.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactCreationTests {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testContactCreationTests() throws Exception {
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("add new")).click();
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys("Petr");
+    driver.findElement(By.name("middlename")).click();
+    driver.findElement(By.name("middlename")).clear();
+    driver.findElement(By.name("middlename")).sendKeys("Petrovich");
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys("Petrov");
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("nickname")).click();
+    driver.findElement(By.name("nickname")).clear();
+    driver.findElement(By.name("nickname")).sendKeys("Petka");
+    driver.findElement(By.name("title")).click();
+    driver.findElement(By.name("title")).clear();
+    driver.findElement(By.name("title")).sendKeys("Petro_file");
+    driver.findElement(By.name("company")).click();
+    driver.findElement(By.name("company")).clear();
+    driver.findElement(By.name("company")).sendKeys("PetroCo");
+    driver.findElement(By.name("address")).click();
+    driver.findElement(By.name("address")).clear();
+    driver.findElement(By.name("address")).sendKeys("Petrov city, Petrovskaya street , 1/10");
+    driver.findElement(By.name("home")).click();
+    driver.findElement(By.name("home")).clear();
+    driver.findElement(By.name("home")).sendKeys("10-10-10");
+    driver.findElement(By.name("mobile")).click();
+    driver.findElement(By.name("mobile")).clear();
+    driver.findElement(By.name("mobile")).sendKeys("1023456789");
+    driver.findElement(By.name("work")).click();
+    driver.findElement(By.name("work")).clear();
+    driver.findElement(By.name("work")).sendKeys("20-20-20");
+    driver.findElement(By.name("fax")).click();
+    driver.findElement(By.name("fax")).clear();
+    driver.findElement(By.name("fax")).sendKeys("30-30-30");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("petro@mail.ru");
+    driver.findElement(By.name("email2")).click();
+    driver.findElement(By.name("email2")).clear();
+    driver.findElement(By.name("email2")).sendKeys("petya@yandex.ru");
+    driver.findElement(By.name("email3")).click();
+    driver.findElement(By.name("email3")).clear();
+    driver.findElement(By.name("email3")).sendKeys("petka@google.com");
+    driver.findElement(By.name("homepage")).click();
+    driver.findElement(By.name("homepage")).clear();
+    driver.findElement(By.name("homepage")).sendKeys("www.petr.ru");
+    driver.findElement(By.name("bday")).click();
+    new Select(driver.findElement(By.name("bday"))).selectByVisibleText("1");
+    driver.findElement(By.name("bmonth")).click();
+    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText("June");
+    driver.findElement(By.name("byear")).click();
+    driver.findElement(By.name("byear")).clear();
+    driver.findElement(By.name("byear")).sendKeys("1990");
+    driver.findElement(By.name("aday")).click();
+    new Select(driver.findElement(By.name("aday"))).selectByVisibleText("10");
+    driver.findElement(By.name("amonth")).click();
+    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText("July");
+    driver.findElement(By.name("ayear")).click();
+    driver.findElement(By.name("ayear")).clear();
+    driver.findElement(By.name("ayear")).sendKeys("2000");
+    driver.findElement(By.name("new_group")).click();
+    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText("test11");
+    driver.findElement(By.name("address2")).click();
+    driver.findElement(By.name("address2")).clear();
+    driver.findElement(By.name("address2")).sendKeys("Petrov city, Petrovskaya street , 1/10");
+    driver.findElement(By.name("phone2")).click();
+    driver.findElement(By.name("phone2")).clear();
+    driver.findElement(By.name("phone2")).sendKeys("10");
+    driver.findElement(By.name("notes")).click();
+    driver.findElement(By.name("notes")).clear();
+    driver.findElement(By.name("notes")).sendKeys("Add new contact");
+    driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    driver.findElement(By.linkText("home")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
