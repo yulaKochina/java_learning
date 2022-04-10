d9b3d190006ea3da0e54c7b7a5e60b24c87c9694
yulaKochina <yulya7@inbox.ru>
2022-04-10 13:16:06 +0300

    Добавлена возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/appManager/ApplicationManager.java
@@ -2,6 +2,9 @@ package ru.yuko.java_learning.addressbook.appManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +15,20 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }else if(browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        }else if(browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.yuko.java_learning.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.yuko.java_learning.addressbook.appManager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
