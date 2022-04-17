cdf8f857c5dede31c782926332fbfeb763e290d3
yulaKochina <yulya7@inbox.ru>
2022-04-17 11:58:44 +0300

    уменьшила таймаут ожидания загрузки страницы

--- addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/appManager/ApplicationManager.java
@@ -29,7 +29,7 @@ public class ApplicationManager {
         }else if(browser.equals(BrowserType.IE)){
             wd = new InternetExplorerDriver();
         }
-        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
