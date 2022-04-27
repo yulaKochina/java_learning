6cbc86a0aae14302de0d3b40176ae597d65d903f
yulaKochina <yulya7@inbox.ru>
2022-04-27 19:47:18 +0300

    в тесты добавлена проверка на совпадение списков контактов по кол-ву записей при создании, удалении и модификации контакта

--- addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/tests/ContactCreationTests.java
@@ -1,9 +1,12 @@
 package ru.yuko.java_learning.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.yuko.java_learning.addressbook.appManager.GroupName;
 import ru.yuko.java_learning.addressbook.model.ContactData;
 import ru.yuko.java_learning.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
@@ -14,9 +17,12 @@ public class ContactCreationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("test11", "test22", "test33"));
     }
     String groupName = app.getGroupHelper().getNameGroup();
+    app.getNavigationHelper().gotoPageHome();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoPageAddNewContact();
     app.getContactHelper().creationContact(new ContactData("Petr", "Petrovich", "Petrov", "Petka", "Petro_file", "PetroCo", "Petrov city, Petrovskaya street , 1/10", "10-10-10", "1023456789", "20-20-20", "30-30-30", "petro@mail.ru", "petya@yandex.ru", "petka@google.com", "www.petr.ru", "1", "June", "1990", "10", "July", "2000", "test11", "Petrov city, Petrovskaya street , 1/10", "10", "Add new contact"), new GroupName(groupName));
     app.getNavigationHelper().gotoPageHome();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size()+1);
   }
-
 }
--- addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/yuko/java_learning/addressbook/tests/ContactModificationTest.java
@@ -1,9 +1,12 @@
 package ru.yuko.java_learning.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.yuko.java_learning.addressbook.appManager.GroupName;
 import ru.yuko.java_learning.addressbook.model.ContactData;
 import ru.yuko.java_learning.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class ContactModificationTest extends TestBase {
     private static String groupName;
@@ -21,10 +24,14 @@ public class ContactModificationTest extends TestBase {
             app.getContactHelper().creationContact(new ContactData("Petr", "Petrovich", "Petrov", "Petka", "Petro_file", "PetroCo", "Petrov city, Petrovskaya street , 1/10", "10-10-10", "1023456789", "20-20-20", "30-30-30", "petro@mail.ru", "petya@yandex.ru", "petka@google.com", "www.petr.ru", "1", "June", "1990", "10", "July", "2000", "test11", "Petrov city, Petrovskaya street , 1/10", "10", "Add new contact"), new GroupName(groupName));
         }
         app.getNavigationHelper().gotoPageHome();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContactCheckbox(before.size()-1);
         app.getContactHelper().initModificationContact();
         app.getContactHelper().fillContactForm(new ContactData("Petr", "Petrovich", "Petrov", "Petka", "Petro_file", "PetroCo", "Petrov city, Petrovskaya street , 1/10", "10-10-10", "1023456789", "20-20-20", "30-30-30", "petro@mail.ru", "petya@yandex.ru", "petka@google.com", "www.petr.ru", "1", "June", "1990", "10", "July", "2000", "test11", "Petrov city, Petrovskaya street , 1/10", "10", "Add new contact"), new GroupName(null));
         app.getContactHelper().submitContactModification();
         app.getNavigationHelper().gotoPageHome();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
 
